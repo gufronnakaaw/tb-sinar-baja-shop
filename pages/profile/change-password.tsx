3c0841711aa012375154f9a95b7ec823ac411f70
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-06-02 15:12:42 +0700

    modified 'create shipping-address' page

--- pages/profile/change-password.tsx
+++ pages/profile/change-password.tsx
@@ -9,7 +9,7 @@ export default function ChangePasswordPage() {
 
   return (
     <Layout title="Ubah Kata Sandi">
-      <div className="grid gap-12">
+      <div className="grid gap-8">
         <header className="grid h-20 grid-cols-[50px_1fr_50px] items-center">
           <Button
             isIconOnly
@@ -25,57 +25,55 @@ export default function ChangePasswordPage() {
           </h5>
         </header>
 
-        <div className="grid gap-8">
-          <div className="grid gap-4">
-            <Input
-              isRequired
-              variant="bordered"
-              color="default"
-              label="Kata Sandi Lama"
-              labelPlacement="outside"
-              endContent={
-                <Key weight="bold" size={18} className="text-foreground-400" />
-              }
-              placeholder="Masukan kata sandi lama"
-            />
+        <div className="grid gap-4">
+          <Input
+            isRequired
+            variant="bordered"
+            color="default"
+            label="Kata Sandi Lama"
+            labelPlacement="outside"
+            endContent={
+              <Key weight="bold" size={18} className="text-foreground-400" />
+            }
+            placeholder="Masukan kata sandi lama"
+          />
 
-            <Input
-              isRequired
-              variant="bordered"
-              color="default"
-              label="Kata Sandi Baru"
-              labelPlacement="outside"
-              endContent={
-                <Key weight="bold" size={18} className="text-foreground-400" />
-              }
-              placeholder="Masukan kata sandi baru"
-            />
+          <Input
+            isRequired
+            variant="bordered"
+            color="default"
+            label="Kata Sandi Baru"
+            labelPlacement="outside"
+            endContent={
+              <Key weight="bold" size={18} className="text-foreground-400" />
+            }
+            placeholder="Masukan kata sandi baru"
+          />
 
-            <Input
-              isRequired
-              variant="bordered"
-              color="default"
-              label="Konfirmasi Kata Sandi Baru"
-              labelPlacement="outside"
-              endContent={
-                <Key weight="bold" size={18} className="text-foreground-400" />
-              }
-              placeholder="Konfirmasi kata sandi baru"
-            />
-          </div>
-
-          <Button
-            color="primary"
-            onClick={() => {
-              if (confirm("Apakah kamu yakin?")) {
-                window.location.href = "/profile";
-              }
-            }}
-            className="font-semibold"
-          >
-            Simpan Kata Sandi
-          </Button>
+          <Input
+            isRequired
+            variant="bordered"
+            color="default"
+            label="Konfirmasi Kata Sandi Baru"
+            labelPlacement="outside"
+            endContent={
+              <Key weight="bold" size={18} className="text-foreground-400" />
+            }
+            placeholder="Konfirmasi kata sandi baru"
+          />
         </div>
+
+        <Button
+          color="primary"
+          onClick={() => {
+            if (confirm("Apakah kamu yakin?")) {
+              window.location.href = "/profile";
+            }
+          }}
+          className="font-semibold"
+        >
+          Simpan Kata Sandi
+        </Button>
       </div>
     </Layout>
   );
--- pages/profile/shipping-address/create.tsx
+++ pages/profile/shipping-address/create.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@nextui-org/react";
+import { Button, Checkbox, Input, Textarea } from "@nextui-org/react";
 import { CaretLeft } from "@phosphor-icons/react";
 import { useRouter } from "next/router";
 
@@ -9,7 +9,7 @@ export default function CreateShippingAddress() {
 
   return (
     <Layout title="Tambah Alamat">
-      <div className="grid gap-12">
+      <div className="grid gap-8">
         <header className="grid h-20 grid-cols-[50px_1fr_50px] items-center">
           <Button
             isIconOnly
@@ -25,7 +25,93 @@ export default function CreateShippingAddress() {
           </h5>
         </header>
 
-        <div>form</div>
+        <div className="grid gap-4">
+          <h4 className="font-semibold text-foreground">Informasi Penerima</h4>
+
+          <Input
+            isRequired
+            variant="bordered"
+            color="default"
+            label="Nama Penerima"
+            labelPlacement="outside"
+            placeholder="Masukan nama penerima"
+          />
+
+          <Input
+            isRequired
+            type="number"
+            variant="bordered"
+            color="default"
+            label="No. Telpon Penerima"
+            labelPlacement="outside"
+            startContent={
+              <span className="text-sm text-foreground-600">+62</span>
+            }
+            placeholder="eg, 8172684294"
+          />
+        </div>
+
+        <div className="grid gap-4">
+          <h4 className="font-semibold text-foreground">Detail Alamat</h4>
+
+          <Input
+            isRequired
+            variant="bordered"
+            color="default"
+            label="Label Alamat"
+            labelPlacement="outside"
+            placeholder="Cth. Alamat Rumah, Alamat Kantor"
+          />
+
+          <Textarea
+            isRequired
+            variant="bordered"
+            color="default"
+            maxRows={4}
+            label="Alamat Lengkap"
+            labelPlacement="outside"
+            placeholder="Masukan nama jalan, no. rumah, dan lain sebagainya"
+          />
+
+          <Input
+            isRequired
+            variant="bordered"
+            color="default"
+            label="Kecamatan, Kota, Provinsi"
+            labelPlacement="outside"
+            placeholder="Cth. Limo, Depok, Jawa Barat"
+          />
+
+          <Input
+            isRequired
+            variant="bordered"
+            color="default"
+            label="Kode Pos"
+            labelPlacement="outside"
+            placeholder="Cth. 16512"
+          />
+
+          <Checkbox
+            color="primary"
+            classNames={{
+              label: "text-sm text-foreground",
+            }}
+          >
+            Jadikan Alamat Utama
+          </Checkbox>
+        </div>
+
+        <Button
+          color="primary"
+          onClick={() => {
+            if (confirm("Apakah kamu yakin?")) {
+              window.location.href = "/profile/shipping-address";
+            }
+          }}
+          className="font-semibold"
+        >
+          Simpan Alamat
+        </Button>
       </div>
     </Layout>
   );
