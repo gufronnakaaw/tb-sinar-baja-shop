9f1f040303de3e5ac81c3ad1eb7a62dd6700e9ec
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-07-20 10:11:01 +0700

    modified CardTransaction components

--- components/card/CardTransaction.tsx
+++ components/card/CardTransaction.tsx
@@ -16,22 +16,26 @@ export default function CardTransaction({
   const router = useRouter();
   return (
     <div
-      className="grid w-full cursor-pointer grid-cols-[40px_1fr_85px] items-center rounded-xl border border-foreground-200 p-4 text-[12px] hover:bg-foreground-100"
+      className="grid w-full cursor-pointer grid-cols-[40px_1fr] items-center gap-2 rounded-xl border border-foreground-200 p-4 hover:bg-foreground-100"
       onClick={() => router.push("/profile/transactions/detail")}
     >
       <Bag size={25} />
 
-      <div className="grid gap-1">
-        <h3 className="text-[13px] font-semibold text-foreground">
-          Transaksi {id_transaksi}
-        </h3>
-        <p>20 Juli 2024 09:13</p>
-        <p className="font-semibold">{total_item} Item</p>
-      </div>
+      <div className="flex items-center justify-between gap-2 text-[12px]">
+        <div className="grid gap-1">
+          <h3 className="text-sm font-semibold text-foreground">
+            Transaksi {id_transaksi}
+          </h3>
+          <p className="font-medium text-foreground-600">20 Juli 2024 09:13</p>
+          <p className="font-semibold text-foreground">{total_item} Item</p>
+        </div>
 
-      <div className="flex flex-col gap-1">
-        <p className="font-semibold">{formatRupiah(total_transaksi)}</p>
-        <p className="font-semibold text-success">{status}</p>
+        <div className="flex flex-col gap-1">
+          <p className="font-semibold text-foreground">
+            {formatRupiah(total_transaksi)}
+          </p>
+          <p className="font-semibold text-success">{status}</p>
+        </div>
       </div>
     </div>
   );
