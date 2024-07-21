f1bb3e4a0dc398803a57a4105686794b9804ea20
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-07-21 15:41:53 +0700

    modified home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import { products } from "@/_dummy/products";
 import Layout from "@/components/Layout";
 import Navbar from "@/components/Navbar";
 import CardProduct from "@/components/card/CardProduct";
-import { Image, Input } from "@nextui-org/react";
-import { MagnifyingGlass } from "@phosphor-icons/react";
+import { Chip, Image } from "@nextui-org/react";
+import { Circle } from "@phosphor-icons/react";
 import NextImage from "next/image";
 import Link from "next/link";
 
@@ -16,32 +16,32 @@ export default function HomePage() {
       <Navbar />
 
       <div className="grid gap-2">
-        <header className="sticky left-0 top-0 z-50 flex h-20 items-center gap-4 bg-white">
-          <Input
-            isRequired
-            variant="bordered"
-            color="default"
-            labelPlacement="outside"
-            startContent={
-              <MagnifyingGlass
-                weight="duotone"
-                size={24}
-                className="text-foreground-400"
-              />
-            }
-            placeholder="Cari produk disini"
-          />
+        <div className="flex items-end justify-between gap-4 pb-4 pt-8">
+          <div>
+            <h3 className="mb-1 text-lg font-bold text-foreground">
+              Selamat Pagi, Gufron.
+            </h3>
+            <p className="max-w-[300px] text-sm font-medium text-foreground-600">
+              Cari Besi Berkualitas Tinggi dan Terpercaya Di Sini.
+            </p>
+          </div>
 
-          {/* <Badge color="danger" content="5">
-            <Button isIconOnly variant="light" color="primary">
-              <ShoppingCart
-                weight="duotone"
-                size={24}
-                className="text-primary"
-              />
-            </Button>
-          </Badge> */}
-        </header>
+          <Chip
+            variant="flat"
+            color="success"
+            size="sm"
+            startContent={
+              <Circle weight="fill" size={7} className="animate-ping" />
+            }
+            className="gap-1"
+            classNames={{
+              base: "px-2",
+              content: "font-semibold",
+            }}
+          >
+            Toko Aktif
+          </Chip>
+        </div>
 
         <div className="mb-2 aspect-video overflow-hidden rounded-xl">
           <Image
