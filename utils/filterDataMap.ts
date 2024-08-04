fc3864aa2ccd9d2ec4eb11b0e13bf29703a8b295
gufronnakaaw <gufronnakaaw@gmail.com>
2024-08-04 23:06:29 +0700

    modified key sorting

--- utils/filterDataMap.ts
+++ utils/filterDataMap.ts
@@ -2,7 +2,7 @@ import { SortingType } from "@/types/filter.type";
 
 export const sorting: SortingType[] = [
   { key: "newest", label: "Terbaru" },
-  { key: "latest", label: "Terlama" },
+  { key: "oldest", label: "Terlama" },
   { key: "highest", label: "Harga Tertinggi" },
   { key: "lowest", label: "Harga Terendah" },
 ];
