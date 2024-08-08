ae954b8b846a11785b9ccaddcafe8ce8393ae94a
gufronnakaaw <gufronnakaaw@gmail.com>
2024-08-08 16:43:14 +0700

    create global declaration type

---
+++ global.d.ts
@@ -0,0 +1,24 @@
+import "next-auth";
+import "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    user_id: string;
+    access_token: string;
+  }
+}
+
+declare module "next-auth" {
+  interface User {
+    user_id: string;
+    access_token: string;
+    id?: string;
+  }
+
+  interface Session {
+    user: {
+      user_id: string;
+      access_token: string;
+    };
+  }
+}
