72666c20d4c250410aa9f76e337a22d156318901
gufronnakaaw <gufronnakaaw@gmail.com>
2024-08-10 20:37:25 +0700

    create empty cart component

---
+++ components/EmptyCart.tsx
@@ -0,0 +1,31 @@
+import { Image } from "@nextui-org/react";
+import NextImage from "next/image";
+import { useRouter } from "next/router";
+
+export default function EmptyCart() {
+  const router = useRouter();
+
+  return (
+    <div className="mt-16 flex flex-col items-center gap-8">
+      <Image
+        priority
+        as={NextImage}
+        src="/img/empty-cart-img.svg"
+        alt="empty box img"
+        width={192}
+        height={192}
+      />
+
+      <div className="grid justify-items-center gap-4">
+        <div className="text-center">
+          <h4 className="mx-auto mb-1 max-w-[230px] font-semibold text-foreground">
+            Oppsss... keranjang kamu masih kosong
+          </h4>
+          <p className="text-[12px] font-medium text-foreground-600">
+            Cari, pesan, dan terima pesanan yang kamu suka.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
