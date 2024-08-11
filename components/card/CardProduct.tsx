5b5cfb2673a23bb165c765f612a9c615da1e8333
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-08-11 10:11:28 +0700

    fix CardProduct components

--- components/card/CardProduct.tsx
+++ components/card/CardProduct.tsx
@@ -12,27 +12,29 @@ export default function CardProduct({ product }: ProductCardProps) {
   return (
     <Link
       href={`/products/${product.slug}`}
-      className="group grid gap-4 rounded-xl border border-foreground-200 bg-white p-4 hover:border-primary"
+      className="group grid gap-4 rounded-[16px] border border-foreground-200 bg-white p-3 hover:border-primary"
     >
-      <Image
-        priority
-        isBlurred
-        as={NextImage}
-        src={`${!product.image.length ? "/img/product-image-test.jpg" : product.image[0].url}`}
-        alt="product img"
-        width={1000}
-        height={1000}
-        className="aspect-square h-auto w-full origin-center rounded-lg object-cover"
-      />
+      <div className="aspect-square overflow-hidden rounded-lg">
+        <Image
+          priority
+          isBlurred
+          as={NextImage}
+          src={`${!product.image.length ? "/img/product-image-test.jpg" : product.image[0].url}`}
+          alt="product img"
+          width={1000}
+          height={1000}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
 
-      <div>
-        <span className="line-clamp-1 text-[12px] text-foreground-600">
+      <div className="overflow-hidden">
+        <span className="line-clamp-1 text-[10px] text-foreground-600">
           {product.kategori}
         </span>
-        <h4 className="mb-3 line-clamp-2 text-sm font-semibold text-foreground group-hover:text-primary">
+        <h4 className="mb-3 line-clamp-2 text-[12px] font-medium text-foreground group-hover:text-primary">
           {product.nama_produk_asli}
         </h4>
-        <h4 className="line-clamp-1 text-sm font-semibold text-primary">
+        <h4 className="line-clamp-1 text-[12px] font-semibold text-primary">
           {formatRupiah(product.harga_6)}
         </h4>
       </div>
